8f51a193032927a4cec23cb1193acb1c97f32692
mayu2312 <131224899+mayu2312@users.noreply.github.com>
2023-05-04 11:57:53 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.dropdown').hover(function() {
+      $(this).addClass('show');
+      $(this).find('.dropdown-menu').addClass('show');
+    }, function() {
+      $(this).removeClass('show');
+      $(this).find('.dropdown-menu').removeClass('show');
+    });
+  });
+  
